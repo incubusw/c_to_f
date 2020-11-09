f3bdd8f272d443d01442920d2357bfaa43030e86
Albert.Wang <Albert.Wang@ADVANTECH.CORP>
2020-11-09 11:49:34 +0800

    The second version

--- c_to_f.py
+++ c_to_f.py
@@ -2,6 +2,9 @@ Temp_F = input('Please input celsius degree:')
 Temp_F = float(Temp_F)
 print('Fahrenheit degree is ', Temp_F * 9 / 5 + 32)
 
+#The second version is to test GitHub function
+
+
 
 
 
